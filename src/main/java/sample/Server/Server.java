c69620b9993b6010ee2c6ee75503b6c8b1a8ab0e
Alexandru <alexandru.sava.1999@gmail.com>
2020-06-13 14:22:47 +0300

    CHAT-42 : Working on server feature

--- src/main/java/sample/Server/Server.java
+++ src/main/java/sample/Server/Server.java
@@ -13,4 +13,27 @@ public class Server {
     public static List<Client> clients;
     public static DataOutputStream dos;
     DataInputStream dis;
+
+    Server() {
+
+        System.out.println("Server");
+
+        String name;
+        Socket client;
+
+        clients = new ArrayList<Client>();
+
+        ServerSocket servSock = new ServerSocket(10001);
+
+        while(true) {
+             client = servSock.accept();
+             dis = new DataInputStream(client.getInputStream());
+             dos = new DataOutputStream(client.getOutputStream());
+             name = dis.readUTF() ;
+
+             Client user = new Client(name, dos, dis);
+             System.out.println("Connected : " + name);
+             clients.add(user);
+        }
+    }
 }

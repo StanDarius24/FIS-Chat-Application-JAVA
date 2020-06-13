42fbb0c3341444bc5184b12e657d559ebbc281d6
Sadovan Bogdan <sadovan.bogdan@yahoo.com>
2020-06-13 15:30:47 +0300

    CHAT-49 Client room

--- src/main/java/Client/room.java
+++ src/main/java/Client/room.java
@@ -23,5 +23,49 @@ public class room {
     @FXML
     public TextArea chatLog;
 
+    public room() {
+        try {
+
+            sock = new Socket(data.ip, data.port);
+            dos = new DataOutputStream(sock.getOutputStream());
+            dis = new DataInputStream(sock.getInputStream());
+
+            dos.writeUTF(data.name);
+            /*
+             * This Thread let the client recieve the message from the server for any time;
+             */
+            th = new Thread(() -> {
+                try {
+
+                    JSONParser parser = new JSONParser();
+
+                    while(true) {
+                        String newMsgJson = dis.readUTF();
+
+                        System.out.println("RE : " + newMsgJson);
+                        Message newMsg = new Message();
+
+                        Object obj = parser.parse(newMsgJson);
+                        JSONObject msg = (JSONObject) obj;
+
+                        newMsg.setName((String) msg.get("name"));
+                        newMsg.setMessage((String) msg.get("message"));
+
+                        chatLog.appendText(newMsg.getName() + " : " + newMsg.getMessage() + "\n");
+                    }
+                } catch(Exception E) {
+                    E.printStackTrace();
+                }
+
+            });
+
+            th.start();
+
+        } catch(IOException E) {
+            E.printStackTrace();
+        }
+
+    }
+
 }
 

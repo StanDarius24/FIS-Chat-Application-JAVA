b2c8d05cb86fa7035434e3b7f9bb6eaa9a67e318
Alexandru <alexandru.sava.1999@gmail.com>
2020-06-13 14:07:39 +0300

    CHAT-39 : Handle and catch exceptions

--- src/main/java/sample/Server/Client.java
+++ src/main/java/sample/Server/Client.java
@@ -1,4 +1,4 @@
-package sample.Server;
+package Server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -21,7 +21,7 @@ public class Client {
 
         new Thread(() -> {
             try {
-                while (true) {
+                while(true) {
                     message = dis.readUTF();
                     System.out.println(message);
                     List<Client> entry = Server.clients;
@@ -31,7 +31,29 @@ public class Client {
                     }
                 }
             } catch (IOException E) {
+                try {
+                    dis.close();
+                    dos.close();
+                    Server.clients = Server.clients.stream()
+                            .filter(e -> {
+                                if(!(e == this)) {
+                                    String exit_message = "{ \"name\" : \"" + "[ SERVER NOTICE ]" + "\", \"message\" : \"" + name + " Disconnected" + "\"}";
+                                    System.out.println(exit_message);
+                                    try {
+                                        e.getDos().writeUTF(exit_message);
+                                    } catch (IOException err) {
+                                        err.printStackTrace();
+                                    }
+                                }
+                                return !(e == this);
+                            })
+                            .collect(Collectors.toList());
 
+                    System.out.println("[Current User : " + Server.clients.size() + "]");
+
+                } catch(IOException E2) {
+                    E2.printStackTrace();
+                }
             }
         }).start();
     }

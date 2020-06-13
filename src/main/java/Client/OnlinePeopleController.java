1cbf231583b1d6581e4d6f783d6eb38d5faa4152
Sadovan Bogdan <sadovan.bogdan@yahoo.com>
2020-06-13 15:37:19 +0300

    CHAT-50 ClientOnlinePeople

---
+++ src/main/java/Client/OnlinePeopleController.java
@@ -0,0 +1,37 @@
+package Client;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class OnlinePeopleController extends Application implements Initializable {
+    private ListView<String> listview ;
+    private static ArrayList<String > chacha = new ArrayList<>();
+    private  ObservableList<String> list ;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Parent root = FXMLLoader.load(OnlinePeopleController.class.getResource("/Client/OnlinePeople.fxml"));
+
+        primaryStage.setTitle("Sclavi online");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}

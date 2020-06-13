c48720dea4c460a363dc0e16f34399a328583dd2
Sadovan Bogdan <sadovan.bogdan@yahoo.com>
2020-06-13 15:25:51 +0300

    CHAT-48 Client login

---
+++ src/main/java/Client/login.java
@@ -0,0 +1,20 @@
+package Client;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class login {
+    @FXML public TextField server_ip;
+    @FXML public TextField port;
+    @FXML public TextField name;
+    @FXML public String sPort;
+
+
+}

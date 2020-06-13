b5cd08c074b5a8f6788e7e2a5aeebc4ee4e4231b
Alexandru <alexandru.sava.1999@gmail.com>
2020-06-13 14:18:01 +0300

    CHAT-41 : Add fields to the class and import libraries

---
+++ src/main/java/sample/Server/Server.java
@@ -0,0 +1,16 @@
+package sample.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    public static List<Client> clients;
+    public static DataOutputStream dos;
+    DataInputStream dis;
+}

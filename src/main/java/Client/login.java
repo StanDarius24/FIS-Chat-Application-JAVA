de277fba85a8e70cdc6918521dae21b70dde4d1c
Sadovan Bogdan <sadovan.bogdan@yahoo.com>
2020-06-13 15:26:35 +0300

    CHAT-48 Client login

--- src/main/java/Client/login.java
+++ src/main/java/Client/login.java
@@ -16,5 +16,28 @@ public class login {
     @FXML public TextField name;
     @FXML public String sPort;
 
+    public void onClick() throws IOException{
+        System.out.println("Clicked");
+        data.ip = server_ip.getText();
+        this.sPort = port.getText();
+        data.name = name.getText();
+        data.port = Integer.parseInt(sPort);
+
+
+        Stage stage;
+        stage = (Stage) server_ip.getScene().getWindow();
+        //Parent parent = FXMLLoader.load(getClass().getResource("room.fxml"));
+        Parent root = FXMLLoader.load(login.class.getResource("room.fxml"));
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setTitle(data.name);
+        stage.setOnCloseRequest(e-> {
+            //e.consume();
+            room.th.stop();
+            System.exit(0);
+        });
+        stage.setResizable(false);
+
+        stage.show();
+    }
 
 }
